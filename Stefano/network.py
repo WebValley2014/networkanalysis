8e8aed81dea00f06eabde67e1611ff301cbc6760
ste96 <stefano.valentini1996@gmail.com>
2014-07-07 18:12:53 +0200

    add pickle part

--- Stefano/network.py
+++ Stefano/network.py
@@ -1,9 +1,19 @@
 from igraph import Graph
+import pickle
+import numpy as np
 
-def drawNetwork(adjacenceMatrix)
+def drawNetwork(srcListMatrix=r'C:\pythontmp\numpyArray.pkl')
     """
+    read the adjacency matrix and plot, thanks to igraph, 
+    the graph of the network
 
+    args:
+    *srcListMatrix*
+        (str)
+        sorce file of the list of matrixs. File '*.pkl' need
+        
     """
-    nw = Graph.Read_AdjacencyGraph.Read_Adjacency(adjacenceMatrix)
-    
-
+    infile1 = open(srcListMatrix, 'r+b')
+    file1 = pickle.load(infile1)                           # This is LISTADIMATRICI
+    infile1.close()
+      
